e9aa0a37be60ef12be1b53f68294dd5e7b4e379e
lidygit <lidy076@163.com>
2018-12-09 18:28:13 +0800

    添加了工艺监控模块

---
+++ src/main/java/com/cskaoyan/erp/bean/VO/TechnologyVO.java
@@ -0,0 +1,20 @@
+package com.cskaoyan.erp.bean.VO;
+
+import com.cskaoyan.erp.bean.Technology;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @Description: java类作用描述
+ * @Author: l
+ * @CreateDate: 2018/12/7 14:45
+ * @需求:
+ * @思路说明:
+ */
+
+@Component
+public class TechnologyVO extends Technology {
+
+
+}
